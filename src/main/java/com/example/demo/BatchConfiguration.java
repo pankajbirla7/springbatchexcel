639d302dbe1a2f20cac9d8c850176b661c0f36f1
pamins <pamins0@gmail.com>
2024-03-27 00:23:13 +0530

    Update BatchConfiguration.java

--- src/main/java/com/example/demo/BatchConfiguration.java
+++ src/main/java/com/example/demo/BatchConfiguration.java
@@ -1,41 +1,104 @@
 package com.example.demo;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import javax.sql.DataSource;
 
-import org.springframework.batch.extensions.excel.RowMapper;
-import org.springframework.batch.extensions.excel.support.rowset.RowSet;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.transaction.PlatformTransactionManager;
 
-public class FinDataCustomRowMapper implements RowMapper<MyDataObject> {
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
 
-    private boolean firstRowSkipped = false;
+	@Autowired
+	private JobRepository jobRepository;
 
-    @Override
-    public MyDataObject mapRow(RowSet rs) throws Exception {
-        if (!firstRowSkipped) {
-            // Skip the first row
-            firstRowSkipped = true;
-            rs.next();
-        }
-        String rowString = rs.getCurrentRow()[2];
-        SimpleDateFormat inputFormat = new SimpleDateFormat("MMddyyyy");
-        SimpleDateFormat outputFormat = new SimpleDateFormat("MM-dd-yyyy");
-        String formattedDateString = "";
-        try {
-            Date date = inputFormat.parse(rowString);
-            formattedDateString = outputFormat.format(date);
-            System.out.println(formattedDateString);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+	@Autowired
+	private PlatformTransactionManager transactionManager;
 
-        MyDataObject dataObject = new MyDataObject();
-        dataObject.setFin(rs.getCurrentRow()[0]);
-        dataObject.setCin(rs.getCurrentRow()[1]);
-        //dataObject.setDateEntered(formattedDateString);
-        dataObject.setNdc(rs.getCurrentRow()[3]);
-        return dataObject;
-        
+	@Autowired
+	private DataSource dataSource;
+
+//    @Autowired
+//    private Step moveFileStep;
+//    
+	@Value("${input.file.paths}")
+	private String inputFiles;
+
+	@Value("${output.file.path}")
+	private String outputFileDirectory;
+
+	// @Bean
+//	public ExcelItemReader excelItemReader() {
+//		return new ExcelItemReader();
+//	}
+
+	@Bean
+	public ExcelItemWriter excelItemWriter() {
+		return new ExcelItemWriter(dataSource);
+	}
+	
+	@Bean
+	public FileDetailsWriter fileDetails() {
+		return new FileDetailsWriter(dataSource);
+	}
+	
+//	@Bean
+//	public ExcelItemWriter agencyDetails() {
+//		return new ExcelItemWriter(dataSource);
+//	}
+
+//	@Bean
+//	public JobExecutionListener listener() {
+//		return new JobCompletionNotificationListener(outputFile);
+//	}
+
+	@Bean
+	public Resource[] inputFiles() {
+		try {
+			ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+			Resource[] resources = resourcePatternResolver.getResources("file:" + inputFiles + "/*.xlsx");
+			return resources;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@Bean
+    public ExcelItemReader customExcelItemReader(Resource[] resources) {
+        return new ExcelItemReader(resources, excelItemWriter(), fileDetails(), outputFileDirectory, dataSource);
     }
+
+	@Bean
+	public Step step1(ItemReader<MyDataObject> reader, ItemWriter<MyDataObject> writer) {
+		return new StepBuilder("step1", jobRepository)
+				.<MyDataObject, MyDataObject>chunk(100000)
+				.reader(customExcelItemReader(inputFiles()))
+				.writer(excelItemWriter())
+				.transactionManager(transactionManager)
+				.build();
+	}
+
+	@Bean
+	public Job importUserJob(Step step1) {
+		return new JobBuilder("importUserJob", jobRepository).
+				incrementer(new RunIdIncrementer())
+				.flow(step1).end()
+				.build();
+	}
 }
