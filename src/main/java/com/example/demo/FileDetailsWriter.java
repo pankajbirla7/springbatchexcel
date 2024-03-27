e0207afbbd51577217b3b40b10c64f73f295e254
pamins <pamins0@gmail.com>
2024-03-27 23:14:36 +0530

    Update FileDetailsWriter.java

--- src/main/java/com/example/demo/FileDetailsWriter.java
+++ src/main/java/com/example/demo/FileDetailsWriter.java
@@ -20,8 +20,8 @@ public class FileDetailsWriter {
 			for (FileDetails item : chunk.getItems()) {
 				if(item.getAgencyFin()!=null) {
 					System.out.println("Writing item: " + item);
-					String sql = "INSERT INTO File (AgencyFEIN, Filename,  SumbitDate) VALUES (?, ?, ?)";
-					jdbcTemplate.update(sql, Integer.parseInt(item.getAgencyFin()), item.getFileName(),  item.getSubmitDate());
+					String sql = "INSERT INTO File (AgencyFEIN, Filename, Submitted_By_Email, SubmitDate) VALUES (?, ?, ?, CURRENT_DATE())";
+					jdbcTemplate.update(sql, Integer.parseInt(item.getAgencyFin()), item.getFileName(),  item.getSubmittedByEmail());
 				}else {
 					continue;
 				}
