2dceb6f458b2f4390661064ae91aa49418717e3f
pamins <pamins0@gmail.com>
2024-03-27 23:13:53 +0530

    scheduling job for every 10 mint and removing command line runner

--- src/main/java/com/example/demo/JobRunner.java
+++ src/main/java/com/example/demo/JobRunner.java
@@ -7,10 +7,11 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JobRunner implements CommandLineRunner {
+public class JobRunner {
 
     private final Job importUserJob;
     private final JobLauncher jobLauncher;
@@ -20,8 +21,8 @@ public class JobRunner implements CommandLineRunner {
         this.jobLauncher = jobLauncher;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    @Scheduled(cron = "0 0/10 * ? * *")
+    public void run() throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
                 .toJobParameters();
