c722066e1f637b47fe3757efd69ece4f857bd7df
Pankaj Birla <pamins0@gmail.com>
2024-05-03 02:27:46 +0530

    Scheduling job in each 20 mint

--- src/main/java/com/example/demo/JobRunner.java
+++ src/main/java/com/example/demo/JobRunner.java
@@ -36,14 +36,14 @@ public class JobRunner {
 		System.out.println("Batch Status: " + batchStatus);
 	}
 
-	@Scheduled(cron = "0 0/1 * ? * *")
+	@Scheduled(cron = "0 0/10 * ? * *")
 	public void prepareFile() throws Exception {
 		System.out.println("Batch Job 2 started ");
 		fileWriteService.generateFile();
 		
 	}
 	
-	@Scheduled(cron = "0 0/2 * ? * *")
+	@Scheduled(cron = "0 0/20 * ? * *")
 	public void downloadFileAndDecrpt() throws Exception {
 		System.out.println("Batch Job 3 started ");
 		fileWriteService.generateFile();
